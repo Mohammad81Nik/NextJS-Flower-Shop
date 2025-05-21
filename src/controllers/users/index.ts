1a3cb05fb65b855d0fc13575f28b03cbdf06b4a7
mohammad <digital.kdan@gmail.com>
2025-05-22 02:54:49 +0330

    Created the initial setup for the users routes

---
+++ src/controllers/users/index.ts
@@ -0,0 +1,24 @@
+import { RequestHandler } from "express";
+
+const getUsers: RequestHandler = async (req, res, next) => {
+  try {
+  } catch (err) {
+    throw err;
+  }
+};
+
+const deleteUserById: RequestHandler = async (req, res, next) => {
+  try {
+  } catch (err) {
+    throw err;
+  }
+};
+
+const editUserById: RequestHandler = async (req, res, next) => {
+  try {
+  } catch (err) {
+    throw err;
+  }
+};
+
+export { getUsers, deleteUserById, editUserById };
